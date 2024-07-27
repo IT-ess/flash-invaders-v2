a108990311eb51c13faa2f9593cb0186f4ec2470
Itess <me@aloiseau.com>
2024-07-27 21:57:42 +0200

    feat: cleaner tutorial

---
+++ src/lib/components/GoToRegistration.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { t } from '$lib/translations/translations';
+	import { Button } from '$lib/components/ui/button';
+	import { Toaster } from '$lib/components/ui/sonner';
+	import { toast } from 'svelte-sonner';
+	import OcticonArrowRight16 from '~icons/octicon/arrow-right-16';
+
+	let buttonDisabled = $state(true);
+
+	$effect(() => {
+		setTimeout(() => {
+			buttonDisabled = false;
+		}, 10000);
+	});
+</script>
+
+<Toaster richColors />
+
+{#if buttonDisabled}
+	<Button variant="outline" class="opacity-50" onclick={() => toast.warning($t('tutorial.warning'))}
+		>{$t('tutorial.register')} <OcticonArrowRight16 class="ml-1" />
+	</Button>
+{:else}
+	<Button href="/{$page.params.lang}/auth" variant="default">
+		{$t('tutorial.register')}
+		<OcticonArrowRight16 class="ml-1" />
+	</Button>
+{/if}
--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -6,73 +6,89 @@
 	import OcticonBook from '~icons/octicon/book';
 	import OcticonArrowBoth from '~icons/octicon/arrow-both';
 	import Fa6SolidMapLocationDot from '~icons/fa6-solid/map-location-dot';
+	import * as Card from '$lib/components/ui/card';
+	import GoToRegistration from '$lib/components/GoToRegistration.svelte';
+	import { PUBLIC_PLATFORM } from '$env/static/public';
+
+	const isMobile = PUBLIC_PLATFORM === 'mobile' ? true : false;
 </script>
 
-<div class="flex flex-col grow w-full justify-center items-center font-sans text-center">
-	<div class="p-6 bg-white min-h-1/4 flex-initial">
-		<p class="font-bold">
-			{$t(`tutorial.welcome.p1`)} <span class="text-blue-600 italic">Zw</span><span
-				class="text-red-500 italic">ie</span
-			><span class="text-black italic">te</span><span class="text-yellow-300 italic">ss</span> !
-			{$t(`tutorial.welcome.p2`)}
-		</p>
-	</div>
-
-	<Accordion.Root class="w-full sm:max-w-[70%]">
-		<Accordion.Item value="item-1">
-			<Accordion.Trigger
-				><div class="w-6 h-6"><OcticonRadioTower /></div>
-				<span class="mr-5">{$t(`tutorial.sec1.heading`)}</span></Accordion.Trigger
+<div class="min-h-screen flex items-center justify-center">
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>{$t(`tutorial.header`)}</Card.Title>
+			<Card.Description
+				><p class="font-bold">
+					{$t(`tutorial.welcome.p1`)} <span class="text-blue-600 italic">Zw</span><span
+						class="text-red-500 italic">ie</span
+					><span class="text-black italic">te</span><span class="text-yellow-300 italic">ss</span> !
+					{$t(`tutorial.welcome.p2`)}
+				</p></Card.Description
 			>
-			<Accordion.Content>
-				<p class="mb-2 text-blue-500 dark:text-blue-400">
-					{$t(`tutorial.sec1.content`)}
-				</p>
-			</Accordion.Content>
-		</Accordion.Item>
-		<Accordion.Item value="item-2">
-			<Accordion.Trigger
-				><div class="w-6 h-6"><OcticonArrowBoth /></div>
-				<span class="mr-5">{$t(`tutorial.sec2.heading`)}</span></Accordion.Trigger
-			>
-			<Accordion.Content>
-				<p class="mb-2 text-blue-500 dark:text-blue-400">
-					{$t(`tutorial.sec2.content`)}
-				</p>
-			</Accordion.Content>
-		</Accordion.Item>
-		<Accordion.Item value="item-3">
-			<Accordion.Trigger
-				><div class="w-6 h-6"><OcticonBrowser /></div>
-				<span class="mr-5">{$t(`tutorial.sec3.heading`)}</span></Accordion.Trigger
-			>
-			<Accordion.Content>
-				<p class="mb-2 text-blue-500 dark:text-blue-400">
-					{$t(`tutorial.sec3.content`)}
-				</p>
-			</Accordion.Content>
-		</Accordion.Item>
-		<Accordion.Item value="item-4">
-			<Accordion.Trigger
-				><div class="w-6 h-6"><OcticonBook /></div>
-				<span class="mr-5">{$t(`tutorial.sec4.heading`)}</span></Accordion.Trigger
-			>
-			<Accordion.Content>
-				<p class="mb-2 text-blue-500 dark:text-blue-400">
-					{$t(`tutorial.sec4.content`)}
-				</p>
-			</Accordion.Content>
-		</Accordion.Item>
-		<Accordion.Item value="item-5">
-			<Accordion.Trigger
-				><div class="w-6 h-6"><Fa6SolidMapLocationDot /></div>
-				<span class="mr-5">{$t(`tutorial.sec5.heading`)}</span></Accordion.Trigger
-			>
-			<Accordion.Content>
-				<p class="mb-2 text-blue-500 dark:text-blue-400">
-					{$t(`tutorial.sec1.content`)}
-				</p>
-			</Accordion.Content>
-		</Accordion.Item>
-	</Accordion.Root>
+		</Card.Header>
+		<Card.Content>
+			<Accordion.Root class="w-full sm:max-w-[70%]">
+				<Accordion.Item value="item-1">
+					<Accordion.Trigger
+						><div class="w-6 h-6"><OcticonRadioTower /></div>
+						<span class="mr-5">{$t(`tutorial.sec1.heading`)}</span></Accordion.Trigger
+					>
+					<Accordion.Content>
+						<p class="mb-2 text-justify dark:text-blue-400">
+							{$t(`tutorial.sec1.content`)}
+						</p>
+					</Accordion.Content>
+				</Accordion.Item>
+				<Accordion.Item value="item-2">
+					<Accordion.Trigger
+						><div class="w-6 h-6"><OcticonArrowBoth /></div>
+						<span class="mr-5">{$t(`tutorial.sec2.heading`)}</span></Accordion.Trigger
+					>
+					<Accordion.Content>
+						<p class="mb-2 text-justify dark:text-blue-400">
+							{$t(`tutorial.sec2.content`)}
+						</p>
+					</Accordion.Content>
+				</Accordion.Item>
+				{#if !isMobile}
+					<Accordion.Item value="item-3">
+						<Accordion.Trigger
+							><div class="w-6 h-6"><OcticonBrowser /></div>
+							<span class="mr-5">{$t(`tutorial.sec3.heading`)}</span></Accordion.Trigger
+						>
+						<Accordion.Content>
+							<p class="mb-2 text-justify dark:text-blue-400">
+								{$t(`tutorial.sec3.content`)}
+							</p>
+						</Accordion.Content>
+					</Accordion.Item>
+				{/if}
+				<Accordion.Item value="item-4">
+					<Accordion.Trigger
+						><div class="w-6 h-6"><OcticonBook /></div>
+						<span class="mr-5">{$t(`tutorial.sec4.heading`)}</span></Accordion.Trigger
+					>
+					<Accordion.Content>
+						<p class="mb-2 text-justify dark:text-blue-400">
+							{$t(`tutorial.sec4.content`)}
+						</p>
+					</Accordion.Content>
+				</Accordion.Item>
+				<Accordion.Item value="item-5">
+					<Accordion.Trigger
+						><div class="w-6 h-6"><Fa6SolidMapLocationDot /></div>
+						<span class="mr-5">{$t(`tutorial.sec5.heading`)}</span></Accordion.Trigger
+					>
+					<Accordion.Content>
+						<p class="mb-2 text-justify dark:text-blue-400">
+							{$t(`tutorial.sec1.content`)}
+						</p>
+					</Accordion.Content>
+				</Accordion.Item>
+			</Accordion.Root>
+		</Card.Content>
+		<Card.Footer class="justify-end">
+			<GoToRegistration />
+		</Card.Footer>
+	</Card.Root>
 </div>
