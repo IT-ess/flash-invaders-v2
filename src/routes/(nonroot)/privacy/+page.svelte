e6abbc921ebeaf436877e9cfdf78a7a05c7f20a7
Itess <me@aloiseau.com>
2024-08-12 15:04:16 +0200

    feat: added privacy page

---
+++ src/routes/(nonroot)/privacy/+page.svelte
@@ -0,0 +1,5 @@
+<p class="text-lg">
+	If you need to delete your data, please send an email to <a
+		href="mailto:comitedejumelagelatessoualle@gmail.com">comitedejumelagelatessoualle@gmail.com</a
+	>
+</p>
