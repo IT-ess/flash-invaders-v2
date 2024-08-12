8d1b042c5f539b302bb9ba6aa59d05c9bf6654c1
Itess <me@aloiseau.com>
2024-08-12 16:02:29 +0200

    feat: added loader on avatar

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -2,6 +2,7 @@
 	import { supabase } from '../supabase-client';
 	import * as Avatar from '$lib/components/ui/avatar';
 	import { downloadAvatar, updateAvatarPath } from '$lib/utils/avatar-functions';
+	import { Circle } from 'svelte-loading-spinners';
 
 	let {
 		url = $bindable(),
@@ -19,7 +20,8 @@
 	let files: FileList | undefined = $state();
 	let classUpload = $state('');
 
-	let classSize = size === 'lg' ? 'h-36 w-36' : 'h-20 w-20';
+	let avatarClassSize = size === 'lg' ? 'h-36 w-36' : 'h-20 w-20';
+	let loaderClassSize = size === 'lg' ? 8 : 6;
 	$effect(() => {
 		classUpload = uploading ? 'opacity-50 bg-slate-600' : '';
 	});
@@ -70,14 +72,18 @@
 		/>
 	</span>
 	<label for="single">
-		<div class="inline-block">
-			<Avatar.Root
-				class="{classSize} mx-auto border-4 border-primary {classUpload}"
-				loadingStatus={uploading ? 'loading' : 'loaded'}
-			>
-				<Avatar.Image src={url} alt={username} />
-				<Avatar.Fallback>{username !== null ? username[0] : 'U'}</Avatar.Fallback>
-			</Avatar.Root>
+		<div class="inline-block box-content">
+			{#if uploading}
+				<Circle color="#FFD93E" size={loaderClassSize} unit="rem" duration="3s" />
+			{:else}
+				<Avatar.Root
+					class="{avatarClassSize} mx-auto border-4 border-primary {classUpload}"
+					loadingStatus={uploading ? 'loading' : 'loaded'}
+				>
+					<Avatar.Image src={url} alt={username} />
+					<Avatar.Fallback>{username !== null ? username[0] : 'U'}</Avatar.Fallback>
+				</Avatar.Root>
+			{/if}
 		</div>
 	</label>
 </div>
