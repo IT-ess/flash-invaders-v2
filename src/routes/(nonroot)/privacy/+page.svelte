323548f71ef0c9304f02d08421bcc2b1eaa703bf
Itess <me@aloiseau.com>
2024-08-12 15:09:34 +0200

    chore: changed privacy wording

--- src/routes/(nonroot)/privacy/+page.svelte
+++ src/routes/(nonroot)/privacy/+page.svelte
@@ -1,5 +1,6 @@
 <p class="text-lg">
-	If you need to delete your data, please send an email to <a
-		href="mailto:comitedejumelagelatessoualle@gmail.com">comitedejumelagelatessoualle@gmail.com</a
+	If you need to delete your data from either this site or the Zwietess application, please send an
+	email to <a href="mailto:comitedejumelagelatessoualle@gmail.com"
+		>comitedejumelagelatessoualle@gmail.com</a
 	>
 </p>
