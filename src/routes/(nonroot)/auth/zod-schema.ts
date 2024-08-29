270a918410297973d7ad5ce8bb19193d856c8232
Itess <me@aloiseau.com>
2024-08-29 14:50:29 +0200

    fix: hanging signin because of required username

--- src/routes/(nonroot)/auth/zod-schema.ts
+++ src/routes/(nonroot)/auth/zod-schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const formSchema = z.object({
 	email: z.string().email('Email invalide'),
 	password: z.string().min(8, 'Mot de passe inférieur à 8 caractères'),
-	username: z.string().min(3, "Nom d'utilisateur inférieur à 3 caractères")
+	username: z.string().min(3, "Nom d'utilisateur inférieur à 3 caractères").optional()
 });
 
 export type FormSchema = typeof formSchema;
