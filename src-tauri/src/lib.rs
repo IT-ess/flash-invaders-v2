2cd20fbd10d49353eda886428c31f933ab0f5491
Itess <me@aloiseau.com>
2024-08-31 15:55:43 +0200

    chore: bring back nfc

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,7 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
-            #[cfg(mobile)]
-            app.handle().plugin(tauri_plugin_nfc::init());
-            Ok(())
-        })
+        .plugin(tauri_plugin_nfc::init())
         .plugin(tauri_plugin_shell::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
