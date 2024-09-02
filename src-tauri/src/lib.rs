9cdfbec26c6f3ddb12926172a0baab0dbfbef846
Itess <me@aloiseau.com>
2024-09-02 19:28:28 +0200

    chore: better nfc plugin init

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,12 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(mobile)]
+            app.handle().plugin(tauri_plugin_nfc::init());
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_nfc::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
