023dd2e9d4df3317aab9d6f1372c4d1a4f9ebb1a
Itess <me@aloiseau.com>
2025-03-21 11:21:59 +0100

    fix: nfc works now !!

---
+++ src-tauri/gen/apple/nfc.swift
@@ -0,0 +1,8 @@
+//
+//  nfc.swift
+//  flash-invaders-v2_iOS
+//
+//  Created by Itess on 21/03/2025.
+//
+
+import CoreNFC
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,11 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(mobile)]
+            app.handle().plugin(tauri_plugin_nfc::init())?;
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
