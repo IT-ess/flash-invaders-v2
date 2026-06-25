b96f3fe9b16ecea8bb2b736983f82c1d5a870b4c
Itess <me@aloiseau.com>
2026-06-25 21:03:43 +0200

    feat: anonymous sign-ins

--- src/routes/(nonroot)/auth/zod-schema.ts
+++ src/routes/(nonroot)/auth/zod-schema.ts
@@ -1,9 +1,7 @@
 import { z } from 'zod';
 
 export const formSchema = z.object({
-	email: z.email('Email invalide'),
-	password: z.string().min(8, 'Mot de passe inférieur à 8 caractères'),
-	username: z.string().min(3, "Nom d'utilisateur inférieur à 3 caractères").optional()
+	username: z.string().min(3, "Nom d'utilisateur inférieur à 3 caractères")
 });
 
 export type FormSchema = typeof formSchema;
